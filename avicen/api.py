6ec033f278b8532b0ee78324a0bdef912794f922
sivajyothis7 <sivajyothis9446@gmail.com>
2024-08-23 18:12:29 +0530

    new

--- avicen/api.py
+++ avicen/api.py
@@ -49,8 +49,8 @@ def fetch_and_create_checkins():
                     previous_log_type = logs_dict[employee_id]['log_type']
                     log_type = "OUT" if previous_log_type == "IN" else "IN"
                 else:
-                    if log_datetime.time() > time(2, 0):  
-                        log_type = "IN"
+                    if log_datetime.time() < time(2, 0):  
+                        log_type = "OUT"
                     # else:
                     #     log_type = "OUT"
 
